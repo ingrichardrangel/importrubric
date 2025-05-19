c904a27bf3f5795a0fb8b993b7daac5eab96be05
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-05-19 16:27:29 -0400

    Add files via upload

--- db/access.php
+++ db/access.php
@@ -1,1 +1,22 @@
+<?php
 
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+
+    'block/importrubric:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ],
+
+    'block/importrubric:myaddinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [],
+    ],
+];
