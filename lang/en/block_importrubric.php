649314ef47e646802e24cb4dcb53cf2e9b818e4f
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-05-19 16:26:52 -0400

    Add files via upload

---
+++ lang/en/block_importrubric.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Import rubric from CSV';
+$string['importfromcsv'] = 'Import rubric from CSV';
+$string['privacy:metadata'] = 'The rubric import block does not store any personal data.';
+$string['onlyintask'] = 'This block is only available within an Assignment activity.';
