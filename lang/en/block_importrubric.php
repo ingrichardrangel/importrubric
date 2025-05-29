f4b1b8e3eaf3dd8e6d19f304fa79cbe04e8a5d1b
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-05-29 18:58:13 -0400

    Add files via upload

--- lang/en/block_importrubric.php
+++ lang/en/block_importrubric.php
@@ -1,5 +1,34 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * handles strings for block_importrubric.
+ *
+ * @package   block_importrubric
+ * @copyright 2025 Richard Rangel
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Language strings for the Import Rubric block.
+
 $string['pluginname'] = 'Import rubric from CSV';
 $string['importfromcsv'] = 'Import rubric from CSV';
 $string['privacy:metadata'] = 'The rubric import block does not store any personal data.';
 $string['onlyintask'] = 'This block is only available within an Assignment activity.';
+
+// Capabilities.
+$string['importrubric:addinstance'] = 'Add a new Import Rubric block to a page';
+$string['importrubric:myaddinstance'] = 'Add a new Import Rubric block to the My Moodle page';
