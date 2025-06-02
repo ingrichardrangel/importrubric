743ff96360f618bb0e2b5df74e86b98bf42bbca0
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-06-02 17:27:52 -0400

    Add files via upload
    Added: $string['errornoid'] = 'No ID Found';

--- lang/en/block_importrubric.php
+++ lang/en/block_importrubric.php
@@ -28,6 +28,7 @@ $string['pluginname'] = 'Import rubric from CSV';
 $string['importfromcsv'] = 'Import rubric from CSV';
 $string['privacy:metadata'] = 'The rubric import block does not store any personal data.';
 $string['onlyintask'] = 'This block is only available within an Assignment activity.';
+$string['errornoid'] = 'No ID Found';
 
 // Capabilities.
 $string['importrubric:addinstance'] = 'Add a new Import Rubric block to a page';
